8b88850853a7a80b65eb777fdd6139b03b4dd6d8
Samuel Davila <samuelrdavila@gmail.com>
2021-02-16 12:10:45 -0600

    initial commit of acquire file

---
+++ acquire.py
@@ -0,0 +1,12 @@
+import pandas as pd
+
+def get_med_insurance_data()
+    """
+    No arguments needed. Run function to acquire raw medical insurance charge data.
+    """
+
+    # acquiring data from local csv downloaded from Kaggle
+    df = pd.read_csv('insurance.csv')
+
+    # returning df
+    return df
